46369bd022de42875a0cfc0502c3e9f1b3abb170
SUAREZ PUGA <DSUAREZPUGA@nacionservicios.com.ar>
2025-06-13 14:59:32 -0300

    Proyecto Inicial De TFG

---
+++ src/main/java/com/siglo21/tfg/TenisappApplication.java
@@ -0,0 +1,13 @@
+package com.siglo21.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siglo21/tfg/TenisappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siglo21.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
